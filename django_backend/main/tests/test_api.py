7a54e0105d755c1bfd08eda9211081de1b2785dd
Vladimir Pokutniy <vovchikp.1989@gmail.com>
2021-08-15 15:06:09 +0300

    tests added

--- django_backend/main/tests/test_api.py
+++ django_backend/main/tests/test_api.py
@@ -1,3 +1,5 @@
+import json
+
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -5,9 +7,12 @@ from rest_framework.test import APITestCase
 from main.models import BaseUser
 from main.serializers import BaseUserSerializer
 
+from django.contrib.auth.models import User
+
 
 class BaseUserApiTestCase(APITestCase):
     def setUp(self):
+        self.user = User.objects.create(username="test_username")
         self.user1 = BaseUser.objects.create(gender="Male", married="No", dependents="0", education="Graduate",
                                              self_employed="No", applicant_income="5849",
                                              coapplicant_income="0.0", loan_amount="1.0", loan_amount_term="360.0",
@@ -46,3 +51,52 @@ class BaseUserApiTestCase(APITestCase):
         print(response.status_code)
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         # self.assertEqual(serializer_data, response.data)
+
+    def test_create(self):
+        self.assertEqual(3, BaseUser.objects.all().count())
+        url = reverse('baseuser-list')
+        data = {
+            "gender": "Male",
+            "married": "No",
+            "dependents": "0",
+            "education": "Graduate",
+            "self_employed": "No",
+            "applicant_income": "5849",
+            "coapplicant_income": "0.0",
+            "loan_amount": "1.0",
+            "loan_amount_term": "360.0",
+            "credit_history": "1.0",
+            "property_area": "Urban",
+            "loan_status": "Y"
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.post(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        # self.assertEqual(serializer_data, response.data)
+        self.assertEqual(4, BaseUser.objects.all().count())
+        self.assertEqual(data['gender'], "Male")
+
+    def test_update(self):
+        url = reverse('baseuser-detail', args=(self.user1.id,))
+        data = {
+            "gender": self.user1.gender,
+            "married": self.user1.married,
+            "dependents": "7",
+            "education": "Graduate",
+            "self_employed": "No",
+            "applicant_income": "5849",
+            "coapplicant_income": "0.0",
+            "loan_amount": "1.0",
+            "loan_amount_term": "360.0",
+            "credit_history": "1.0",
+            "property_area": "Urban",
+            "loan_status": "Y"
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.put(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.user1 = BaseUser.objects.get(id=self.user1.id)
+        # self.user1.refresh_from_db()
+        self.assertEqual("7", self.user1.dependents)
--- django_backend/main/views/base/base_user.py
+++ django_backend/main/views/base/base_user.py
@@ -3,14 +3,15 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 from main.models import BaseUser
 from main.serializers.base import BaseUserSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 
 class BaseUserViewSet(ModelViewSet):
     queryset = BaseUser.objects.all()
     serializer_class = BaseUserSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     filter_fields = ['loan_amount']
     search_fields = ['education', 'property_area']
     ordering_fields = ['loan_amount', 'loan_amount_term']
