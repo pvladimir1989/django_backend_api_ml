6ac8e55ff7bfc28611649467c22ad11feca81a72
Vladimir Pokutniy <vovchikp.1989@gmail.com>
2021-08-16 18:06:19 +0300

    tests need checking and fixing before merge

--- django_backend/main/views/base/base_user.py
+++ django_backend/main/views/base/base_user.py
@@ -2,6 +2,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 from main.models import BaseUser
+from main.permissions import IsUserOrStaffOrReadOnly
 from main.serializers.base import BaseUserSerializer
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
@@ -11,7 +12,8 @@ class BaseUserViewSet(ModelViewSet):
     serializer_class = BaseUserSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     # permission_classes = [IsAuthenticated]
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    # permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsUserOrStaffOrReadOnly]
     filter_fields = ['loan_amount']
     search_fields = ['education', 'property_area']
     ordering_fields = ['loan_amount', 'loan_amount_term']
